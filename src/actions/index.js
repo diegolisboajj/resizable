1624caff7bcda21fa3ecf19b4f2397ec29714d2d
Bodi44 <shtohrinets@ucu.edu.ua>
2019-04-04 14:00:55 +0300

    change readme.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
 import { v4 } from 'node-uuid'
-
+//TODO change actions names:
 export const ADD_ARTICLE = 'ADD_ARTICLE'
 export const REMOVE_ARTICLE = 'REMOVE_ARTICLE'
-export const ADD_TO_LAYOUT = 'ADD_TO_LAYOUT'
-export const REMOVE_FROM_LAYOUT = 'REMOVE_FROM_LAYOUT'
-export const ADD_NEW_ROW = 'ADD_NEW_ROW'
-export const ADD_TO_EXISTING_ROW = 'ADD_TO_EXISTING_ROW'
-export const DELETE_ROW = 'DELETE_ROW'
-export const CHANGE_COL = 'CHANGE_COL'
-export const UPDATE_SIZE = 'UPDATE_SIZE'
-export const MOVE_ARTICLE = 'MOVE_ARTICLE'
+export const ADD_ARTICLE_TO_LAYOUT = 'ADD_ARTICLE_TO_LAYOUT'
+export const REMOVE_ARTICLE_FROM_LAYOUT = 'REMOVE_ARTICLE_FROM_LAYOUT'
+export const ADD_NEW_ROW_TO_LAYOUT = 'ADD_NEW_ROW_TO_LAYOUT'
+export const ADD_ARTICLE_TO_EXISTING_ROW = 'ADD_ARTICLE_TO_EXISTING_ROW'
+export const DELETE_ROW_FROM_LAYOUT = 'DELETE_ROW_FROM_LAYOUT'
+export const CHANGE_ARTICLE_COLUMN_IN_ROW = 'CHANGE_ARTICLE_COLUMN_IN_ROW'
+export const UPDATE_ARTICLE_SIZE_IN_LAYOUT = 'UPDATE_ARTICLE_SIZE_IN_LAYOUT'
+export const REORDER_ARTICLE_IN_ROW = 'REORDER_ARTICLE_IN_ROW'
 
 export const addArticle = text => ({
   type: ADD_ARTICLE,
@@ -22,8 +22,8 @@ export const removeArticle = id => ({
   id
 })
 
-export const addToLayout = ({ id, col, row, size }) => ({
-  type: ADD_TO_LAYOUT,
+export const addArticleToLayout = ({ id, col, row, size }) => ({
+  type: ADD_ARTICLE_TO_LAYOUT,
   id,
   col,
   row,
@@ -31,41 +31,41 @@ export const addToLayout = ({ id, col, row, size }) => ({
 })
 
 
-export const removeFromLayout = id => ({
-  type: REMOVE_FROM_LAYOUT,
+export const removeArticleFromLayout = id => ({
+  type: REMOVE_ARTICLE_FROM_LAYOUT,
   id
 })
 
-export const addNewRow = () => ({
-  type: ADD_NEW_ROW,
+export const addNewRowToLayout = () => ({
+  type: ADD_NEW_ROW_TO_LAYOUT,
   id: v4()
 })
 
-export const addToExistingRow = ({ id, rowId }) => ({
-  type: ADD_TO_EXISTING_ROW,
+export const addArticleToExistingRow = ({ id, rowId }) => ({
+  type: ADD_ARTICLE_TO_EXISTING_ROW,
   id,
   rowId
 })
 
-export const deleteRow = id => ({
-  type: DELETE_ROW,
+export const deleteRowFromLayout = id => ({
+  type: DELETE_ROW_FROM_LAYOUT,
   id
 })
 
-export const changeCol = ({ id, col }) => ({
-  type: CHANGE_COL,
+export const changeArticleColumnInRow = ({ id, col }) => ({
+  type: CHANGE_ARTICLE_COLUMN_IN_ROW,
   id,
   col
 })
 
-export const updateSize = ({ id, size }) => ({
-  type: UPDATE_SIZE,
+export const updateArticleSizeInLayout = ({ id, size }) => ({
+  type: UPDATE_ARTICLE_SIZE_IN_LAYOUT,
   id,
   size
 })
 
-export const moveArticle = (rowId, index, overIndex, draggedId, overId) => ({
-  type: MOVE_ARTICLE,
+export const reorderArticleInRow = (rowId, index, overIndex, draggedId, overId) => ({
+  type: REORDER_ARTICLE_IN_ROW,
   rowId: rowId,
   index: index,
   overIndex: overIndex,
